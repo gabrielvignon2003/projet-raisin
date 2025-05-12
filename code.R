70143a86520c90545731af14512838fe726e58e8
Gabriel Vignon <gabriel.vignon@ensta-paris.fr>
2025-05-12 12:18:00 +0200

    début de l'analyse descriptive

--- code.R
+++ code.R
@@ -1,1 +1,9 @@
-#blibloubla
+# Importation des bibliothèques
+library(ggplot2)
+library(readxl)
+
+# Partie 1 : analyse non supervisée
+data <- read_excel("Raisin.xlsx")
+entete <- head(data)
+resume <- summary(data)
+plot(data)
